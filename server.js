3e5b53fb6595970c2feaa013d640e81d01de1e4a
lakshmanan-sona <lakshmanan.sona@gmail.com>
2017-08-05 11:11:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-first', function(req, res){
+   res.send('Article First will be displayed here'); 
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
